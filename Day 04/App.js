f54a24f32a407becd1d21889bb191d6616c88a05
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-06 12:39:57 +0000

    we are creating app- Food Villa

--- Day 04/App.js
+++ Day 04/App.js
@@ -1,13 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => {
+/***
+ * Food Villa - Layout
+ * *********************************
+ * Header
+ *    - Logo
+ *    - Nav Items (Right Side)
+ *    - Cart
+ * **********************************
+ *
+ * * *********************************
+ * Body
+ *    - Search Bar
+ *    - Resturant List
+ *    - Resturant Card
+ *        - Image
+ *        - Name
+ *        - Cusine
+ *        - Rating
+ * **********************************
+ *
+ * * *********************************
+ * Footer
+ *    - Links
+ *    - Copyright
+ * **********************************
+ */
+
+// Logo
+const Logo = () => {
   return (
-    <div>
-      <h1>Food-Villa</h1>
+    <a href="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://scontent.fccu19-1.fna.fbcdn.net/v/t39.30808-6/245514136_344214294165882_1239671097814191122_n.png?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=GXE6wm2BukwAX-Y0Mep&_nc_ht=scontent.fccu19-1.fna&oh=00_AfAcLMVQxu9fbPbJVSJ14HZ2kDdfsbfe01XMv-agaEymdw&oe=640A6AE7"
+      />
+    </a>
+  );
+};
+
+// Header Component
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Logo />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
+const Body = () => {};
+
+// React Fragment - <React.Fragment> <React.Fragment/>  or <>we can use empty tag</>
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Title />);
+root.render(<HeaderComponent />);

16ca70d5b3f4968a7da142ce363726ed389c1673
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-10 15:49:39 +0000

    learned UseState

--- Day 05/food-villa/src/components/Body.js
+++ Day 05/food-villa/src/components/Body.js
@@ -1,5 +1,6 @@
-import { RestaurantLists } from "../constants";
+import { ResturantLists } from "../constants";
 import ResturantCard from "./ResturantCard";
+import { useState } from "react";
 
 /**
  * key -
@@ -8,14 +9,53 @@ import ResturantCard from "./ResturantCard";
  * No KEY is not Acceptable.
  */
 
+// what is State?
+// what is useState? - will return [variable Name, function to update variable]
+// what is Hooks? - function
+
+// Filter Data of Resturant
+function filterData(searchText, resturants) {
+  const filterData = resturants.filter((resturant) =>
+    resturant.data.name.includes(searchText)
+  );
+  return filterData;
+}
+
 // Body Component
 const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [resturants, setResturants] = useState(ResturantLists);
+
   return (
-    <div className="resturant-lists">
-      {RestaurantLists.map((restaurant) => {
-        return <ResturantCard {...restaurant.data} key={restaurant.data.id} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="searchbtn"
+          onClick={() => {
+            // need to filter data
+            const data = filterData(searchText, resturants);
+            // update the state
+            setResturants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="resturant-lists">
+        {resturants.map((resturant) => {
+          return <ResturantCard {...resturant.data} key={resturant.data.id} />;
+        })}
+      </div>
+    </>
   );
 };
 

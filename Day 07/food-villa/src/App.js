f59f4c0d81d5f25bfb2016275bf616cffc2d672e
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-12 16:11:58 +0000

    imported createBrowserRouter, RouterProvider from react-router-dom

--- Day 07/food-villa/src/App.js
+++ Day 07/food-villa/src/App.js
@@ -3,50 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
-/**
- * Default import
- * import Header from "./components/Header";
- * import NewHeader from "./components/Header";
- *    We can rename it to a different value
- *
- * Named import(s)
- * import { Title } from "./components/Header";
- *    Example createRoot import style
- *
- * import * as Obj from "./components/Header";
- *    <Obj.Title /> Example of React.Fragment
- * **/
-
-/***
- * Food Villa - Website Design
- * ============================
- * Header
- *    - Logo
- *    - Nav Items (Right Side)
- *    - Cart
- * ============================
- *
- * =============================
- * Body
- *    - Search Bar
- *    - Resturant List
- *    - Resturant Cards
- *        - Image
- *        - Name
- *        - Cusine
- *        - Rating
- * ==============================
- *
- * ==============================
- * Footer
- *    - Links
- *    - Copyright
- * ==============================
- */
-
-// React Fragment - <React.Fragment> <React.Fragment/>  or <>we can use empty tag</>
-// JSX - have only one parent that's why we use React Fragment or div or empty tag
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -58,5 +16,16 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);

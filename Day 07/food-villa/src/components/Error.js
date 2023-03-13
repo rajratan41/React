4f4d98eb1ed5fa82db3711e4500b44163142d6b6
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-13 04:37:19 +0000

    display reason of error using useRouterError from react-router-dom

--- Day 07/food-villa/src/components/Error.js
+++ Day 07/food-villa/src/components/Error.js
@@ -1,8 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+  const err = useRouteError();
+  const { data, status, statusText } = err;
+  // console.log(err);
   return (
     <div>
       <h1>Oops!!</h1>
       <h2>Something went wrong!!</h2>
+      <h2>{data}</h2>
+      <h2>{status + " : " + statusText}</h2>
     </div>
   );
 };

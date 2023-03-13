d55e2802b345e0e3490e22ddab029fb945fb93be
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-13 10:05:58 +0000

    added comment for useParam() hook

--- Day 07/food-villa/src/components/ResturantMenu.js
+++ Day 07/food-villa/src/components/ResturantMenu.js
@@ -1,15 +1,21 @@
 import { useParams } from "react-router-dom";
 
 const ResturantMenu = () => {
+  /**
+   * how to read the dynamic URL
+   * using useParams() hook from react-router-dom
+   */
+
   //   const parms = useParams();
   //   const { id } = parms;
   //   console.log(parms);
+
   //   or you can direct distructure
-  const { id } = useParams();
+  const { resid } = useParams();
 
   return (
     <div>
-      <h1>Resturant Id: {id}</h1>
+      <h1>Resturant Id: {resid}</h1>
       <h1> Pizza Hut </h1>
     </div>
   );

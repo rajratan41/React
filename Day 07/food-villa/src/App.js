f0490c21189a075e00ed37e0b4730210a7d7daa0
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-13 04:13:04 +0000

    creating error element in react router dom

--- Day 07/food-villa/src/App.js
+++ Day 07/food-villa/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
@@ -20,6 +21,7 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: "/about",

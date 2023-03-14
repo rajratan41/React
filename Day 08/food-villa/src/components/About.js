04654c78a79ea23edba21c93f74bcc14f9ada942
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-14 10:06:51 +0000

    Created Child Route of About and Creating Profile Component

--- Day 08/food-villa/src/components/About.js
+++ Day 08/food-villa/src/components/About.js
@@ -1,8 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Profile from "./Profile";
 const About = () => {
   return (
     <div>
       <h2>About us Page</h2>
       <p>we are learning how to create path</p>
+      {/* <Outlet/> */}
+      <Profile />
     </div>
   );
 };

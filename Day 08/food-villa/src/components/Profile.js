cb0b6c5f18887328701789ddbf1f0a95f27078d7
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-14 15:16:53 +0000

    created functional component

--- Day 08/food-villa/src/components/Profile.js
+++ Day 08/food-villa/src/components/Profile.js
@@ -1,7 +1,34 @@
-const Profile = () => {
+import { useEffect, useState } from "react";
+/**
+ * First render
+ * then API Call (useEffect())
+ * then again render
+ */
+
+const Profile = (props) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  useEffect(() => {
+    // API Call
+    // console.log("useEffect");s
+  });
+
+  // console.log("render");
+
   return (
     <div>
-      <h1>Profile Component</h1>
+      <h1>Profile - Functional Component</h1>
+      <h3>Name: {props.name}</h3>
+      <h3>Count: {count}</h3>
+      <h3>Count2: {count2}</h3>
+      <button
+        onClick={() => {
+          setCount(1), setCount2(2);
+        }}
+      >
+        Count
+      </button>
     </div>
   );
 };

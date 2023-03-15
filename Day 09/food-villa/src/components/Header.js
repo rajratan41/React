b2de996dc36c464d182ccc6398aafa3ae6fd9f28
Raj Ratan <raj808341@gmail.com>
2023-03-15 14:21:00 +0530

    added Logo

---
+++ Day 09/food-villa/src/components/Header.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../assets/img/foodVilla.png";
+
+const loggedInUser = () => {
+  // API call to check Authentication
+  return false;
+};
+
+// Single Page Application (SPA) ?
+// client side Routing
+
+// Logo
+const Title = () => (
+  <Link to="/">
+    <img alt="logo" src={Logo} className="logo" />
+  </Link>
+);
+
+// Header Component
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+
+          <li>
+            <Link to="cart">Cart</Link>
+          </li>
+        </ul>
+      </div>
+      {isLoggedIn ? (
+        <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLoggedIn(true)}>Login</button>
+      )}
+    </div>
+  );
+};
+
+export default Header;

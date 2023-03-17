310836d3599f2e52bd96f9c82758ae4eb1eccd0e
Raj Ratan <raj808341@gmail.com>
2023-03-17 16:01:43 +0530

    spelling fix

--- Day 09/food-villa/src/components/RestaurantMenu.js
+++ Day 09/food-villa/src/components/RestaurantMenu.js
@@ -18,25 +18,25 @@ const RestaurantMenu = () => {
     <div className="menu">
       <div>
         <h1>restaurant id: {resId}</h1>
-        <h2>{resturant?.cards[0]?.card?.card?.info?.name}</h2>
+        <h2>{restaurant?.cards[0]?.card?.card?.info?.name}</h2>
         <img
           src={
             IMG_CDN_URL +
-            resturant?.cards[0]?.card?.card?.info?.cloudinaryImageId
+            restaurant?.cards[0]?.card?.card?.info?.cloudinaryImageId
           }
         />
-        <h3>{resturant?.cards[0]?.card?.card?.info?.locality}</h3>
-        <h3>{resturant?.cards[0]?.card?.card?.info?.city}</h3>
-        <h3>{resturant?.cards[0]?.card?.card?.info?.avgRatingString} Stars</h3>
-        <h3>{resturant?.cards[0]?.card?.card?.info?.costForTwoMessage}</h3>
+        <h3>{restaurant?.cards[0]?.card?.card?.info?.locality}</h3>
+        <h3>{restaurant?.cards[0]?.card?.card?.info?.city}</h3>
+        <h3>{restaurant?.cards[0]?.card?.card?.info?.avgRatingString} Stars</h3>
+        <h3>{restaurant?.cards[0]?.card?.card?.info?.costForTwoMessage}</h3>
       </div>
       <div>
         <h1>Menu</h1>
         <ul>
           {Object.values(
-            resturant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
+            restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]
               ?.card?.card?.itemCards
-          ).map((item) => {
+          ).map((items) => {
             return (
               <li key={items?.card?.info?.id}>{items?.card?.info?.name}</li>
             );

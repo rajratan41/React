3b1fd3967195a96076a68df8220e51cbc03218a0
Raj Ratan <raj808341@gmail.com>
2023-03-19 21:15:37 +0530

    updating instamart component - using Props Drilling

--- Day 11/food-villa/src/components/Instamart.js
+++ Day 11/food-villa/src/components/Instamart.js
@@ -1,8 +1,37 @@
+// Props Drilling
+const Section = ({ title, description }) => {
+  return (
+    <div className="mt-8 border border-black w-5/6">
+      <h1 className="font-semibold text-2xl p-3">{title}</h1>
+      <p className="font-medium text-2xl p-3">{description}</p>
+    </div>
+  );
+};
+
 const Instamart = () => {
   return (
-    <div>
-      <h1>Instamart</h1>
-      <h1>100s of Component inside it !!!</h1>
+    <div className="flex flex-col items-center justify-center bg-pink-200 font-[Poppins]">
+      <h1 className="text-6xl font-semibold p-4 m-1">Instamart</h1>
+      <Section
+        title={"About Instamart"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+      />
+
+      <Section
+        title={"Team Instamart"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+      />
+
+      <Section
+        title={"Careers"}
+        description={
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+        }
+      />
     </div>
   );
 };

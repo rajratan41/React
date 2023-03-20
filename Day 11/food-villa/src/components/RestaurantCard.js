355f22f83b7508ac59061ec43fd0d9c0f96af32c
Raj Ratan <raj808341@gmail.com>
2023-03-20 20:53:56 +0530

    uses useContext inside card

--- Day 11/food-villa/src/components/RestaurantCard.js
+++ Day 11/food-villa/src/components/RestaurantCard.js
@@ -1,4 +1,6 @@
 import { IMG_CDN_URL } from "../constants";
+import { useContext } from "react";
+import UserContext from "../utils/UserContext";
 
 // restaurant Card Component
 const RestaurantCard = ({
@@ -7,8 +9,10 @@ const RestaurantCard = ({
   cloudinaryImageId,
   totalRatingsString,
 }) => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div className=" rounded-lg bg-white h-[18rem] w-[15rem] font-[Montserrat]  text-black shadow-xl transition ease-out duration-75 hover:scale-95">
+    <div className=" rounded-lg bg-white  w-[15rem] font-[Montserrat]  text-black shadow-xl transition ease-out duration-75 hover:scale-95">
       <img
         className="mt-0 rounded-t-lg"
         src={IMG_CDN_URL + cloudinaryImageId}
@@ -16,6 +20,9 @@ const RestaurantCard = ({
       <h2 className="font-bold mt-2 text-center">{name}</h2>
       <h5 className="mt-2 text-sm text-center">{cuisines.join(", ")}</h5>
       <h3 className="mt-2 text-sm text-center">{totalRatingsString}</h3>
+      <h5 className="font-semibold text-red-900">
+        {user.name} - {user.email}
+      </h5>
     </div>
   );
 };

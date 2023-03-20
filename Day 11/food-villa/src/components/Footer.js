f85b7bfe61adac9215e18549fe6629ff6480d8f2
Raj Ratan <raj808341@gmail.com>
2023-03-20 21:06:48 +0530

    useContext in Footer

--- Day 11/food-villa/src/components/Footer.js
+++ Day 11/food-villa/src/components/Footer.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import UserContext from "../utils/UserContext";
+
 // Footer Component
 const Footer = () => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div className="flex justify-center items-center p-9 bg-pink-200 font-[Poppins]">
-      <h3>Made with ❤️ Raj Ratan </h3>
+    <div className="flex justify-center items-center font-semibold text-red-900 p-9 bg-pink-200 font-[Poppins]">
+      <h3> This site is developed with ❤️ by {user.name} </h3>
     </div>
   );
 };

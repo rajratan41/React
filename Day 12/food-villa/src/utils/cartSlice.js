774dd8927dfff913f0e22be28bbfad574fb0c09a
Raj Ratan <raj808341@gmail.com>
2023-03-22 14:49:52 +0530

    created cartSlice and exporting actions and reducer

--- Day 12/food-villa/src/utils/cartSlice.js
+++ Day 12/food-villa/src/utils/cartSlice.js
@@ -1,3 +1,24 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
-const cartSlice = createSlice();
+// creating cartSlice
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: ["Banana", "Apple"],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;

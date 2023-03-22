5e46d18afe5e2b23d098ef723b937d85512a192f
Raj Ratan <raj808341@gmail.com>
2023-03-22 20:49:57 +0530

    console log cartItem

---
+++ Day 12/food-villa/src/components/FoodItem.js
@@ -0,0 +1,9 @@
+const FoodItem = (items) => {
+  return (
+    <div className=" rounded-lg bg-white  w-[15rem] font-[Montserrat]  text-black shadow-xl transition ease-out duration-75 hover:scale-95">
+      <h2 className="font-bold mt-2 text-center"></h2>
+    </div>
+  );
+};
+
+export default FoodItem;
